609cdc8b2c2c5937cc9d2e5368d11e1cf02c41cc
Levi Neely <141506390+lneely@users.noreply.github.com>
2024-09-08 18:33:24 +0200

    admin crap

--- vfs/vfs-ops-b.el
+++ vfs/vfs-ops-b.el
@@ -1,7 +1,26 @@
 ;;; vfs-ops-b.el --- Operation handlers for /b directory in VFS
 
-;; This file contains the operation handlers for the /b directory in
-;; the Virtual File System.
+;;; Commentary:
+;;;
+;;; This file is not part of GNU Emacs.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;; This file contains the operation handlers for the /b directory in
+;;; the Virtual File System.
+
+;;; Code:
 
 (require 'vfs)
 (require 'vfs-rpc)
--- vfs/vfs-rpc.el
+++ vfs/vfs-rpc.el
@@ -1,4 +1,26 @@
-;; rpc.el -- defines the RPC functionality exposed by the vfs
+;;; rpc.el -- defines the RPC functionality exposed by the vfs
+
+;;; Commentary:
+;;;
+;;; This file is not part of GNU Emacs.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;; This file contains the operation handlers for the /b directory in
+;;; the Virtual File System.
+
+;;; Code:
 
 (defun vfs-rpc-list-buffers ()
   "List all current buffers as if they are files in a directory."
@@ -16,3 +38,5 @@
     (buffer-string)))
 
 (provide 'vfs-rpc)
+
+;;; vfs-rpc.el ends here.
